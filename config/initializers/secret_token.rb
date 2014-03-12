9ef3be8a6e139ef8f097d09d701d02ff58f32666
Svante <svantehaggblom@gmail.com>
2014-03-12 20:55:47 +0200

    eka committi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Velkakirja::Application.config.secret_key_base = '1dd0c5ecd153fd98f1ac6099013c3dbf151655029b51be9693d1fe555b199001e55618d4f40371a35735cedd5107fc1a8980c8584e6d9cbb5ce35700b5c07e3b'
