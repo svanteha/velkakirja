1d7105606b64fbf5582acd9f0b79076a9d203758
Svante <svantehaggblom@gmail.com>
2014-03-19 16:05:20 +0200

    jotain juu

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ActiveRecord::Base
-  include DebtsTotalSum
 
   belongs_to :apartment
   has_many :debts, dependent: :destroy
-
-  def count_debts
-    debts.sum_value :amount
-  end
 
 end
